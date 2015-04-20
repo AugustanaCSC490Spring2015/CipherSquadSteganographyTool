b10bc3d9cbd0e3ba5f23cebce50fe816ee57224d
daniellebryant <daniellebryant12@augustana.edu>
2015-04-20 14:45:28 -0500

    Added encode and decode activities

--- app/src/main/java/edu/augustana/csc490/steganographytool/MainActivity.java
+++ app/src/main/java/edu/augustana/csc490/steganographytool/MainActivity.java
@@ -1,9 +1,14 @@
 package edu.augustana.csc490.steganographytool;
 
+
+import android.content.Intent;
+import android.view.View;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,15 +17,42 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button encodeButton = (Button) findViewById(R.id.encodeButton);
+        encodeButton.setOnClickListener(encodeButtonListener);
+
+        Button decodeButton = (Button) findViewById(R.id.decodeButton);
+        decodeButton.setOnClickListener(decodeButtonListener);
     }
+
+    View.OnClickListener encodeButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent encodeIntent = new Intent(MainActivity.this, encode.class);
+            startActivity(encodeIntent);
+        }
+    };
+
+    View.OnClickListener decodeButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent decodeIntent = new Intent(MainActivity.this, decode.class);
+            startActivity(decodeIntent);
+        }
+    };
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+
         return true;
+
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -36,4 +68,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
