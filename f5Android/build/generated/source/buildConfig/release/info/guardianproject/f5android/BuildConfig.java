c0110b09ca5f0adf82a6395aca3c1323de0049ae
Jonathon Meyer <jonathonmeyer11@augustana.edu>
2015-04-28 13:33:07 -0500

    f5android library

---
+++ f5Android/build/generated/source/buildConfig/release/info/guardianproject/f5android/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package info.guardianproject.f5android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "info.guardianproject.f5android";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
